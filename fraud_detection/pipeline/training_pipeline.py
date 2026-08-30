145cec36188ef8d35689177ecec1758a2dc479ae
Aditya Ranganekar <ranganekaraditya@gmail.com>
2026-08-30 19:35:11 +0530

    added s3 sync methods for artifacts and model

--- fraud_detection/pipeline/training_pipeline.py
+++ fraud_detection/pipeline/training_pipeline.py
@@ -10,6 +10,9 @@ from fraud_detection.components.data_validation import DataValidation
 from fraud_detection.components.data_transformation import DataTransformation
 from fraud_detection.components.model_trainer import ModelTrainer
 
+from fraud_detection.constant.training_pipeline import TRAINING_BUCKET_NAME, SAVED_MODEL_DIR
+from fraud_detection.cloud.s3_syncer import S3Sync
+
 from fraud_detection.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from fraud_detection.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact
 
@@ -18,6 +21,7 @@ class TrainingPipeline:
 
     def __init__(self):
         self.training_pipeline_config= TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
     
     def start_data_ingestion(self):
         try:
@@ -90,6 +94,23 @@ class TrainingPipeline:
         except Exception as e:
             raise FraudDetectionException(e, sys)
 
+
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir, aws_bucket_url = aws_bucket_url)
+        except Exception as e:
+            raise FraudDetectionException(e,sys)
+        
+        
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.model_dir, aws_bucket_url = aws_bucket_url)
+        except Exception as e:
+            raise FraudDetectionException(e,sys)
+        
+
     def run_pipeline(self):
         try:
             pipeline_start_time = time.time()
@@ -97,6 +118,9 @@ class TrainingPipeline:
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact=self.start_model_training(data_transformation_artifact=data_transformation_artifact)
+
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
             
             pipeline_end_time = time.time()
             total_time = pipeline_end_time - pipeline_start_time
